1c73102cd3036b9f50fc6f5ae864f5a2ec267321
jsanin <jsanin@growthaccelerationpartners.com>
2023-01-17 09:01:15 -0500

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { AddCategory,GifGrid } from "./componentes";
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory= (newCategory)=>{
+        if (categories.includes(newCategory)) return;
+        setCategories([newCategory,...categories]);
+        //console.log(newCategory);
+
+    }
+
+  return (
+    <>
+    
+    <h1>GifExpertApp</h1>
+
+    <AddCategory 
+    onNewCategory={ (event)=>onAddCategory(event)}
+    />
+
+   
+        {
+        categories.map((category)=>(
+
+                <GifGrid 
+                    key={category}
+                    category={category}>
+
+                 </GifGrid>
+
+            ) )
+        
+        }
+
+    </>
+  )
+}
